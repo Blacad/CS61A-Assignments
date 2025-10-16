19b18f6885316d22e531c77aaa03ca38a0700159
Blacad <2892045788@qq.com>
2025-10-16 19:34:51 +0800

    完成lab8-10

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,10 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond((> num1 num2) 1) ((< num1 num2) -1) (else 0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (x) (+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) (define (helper x n) (if (> n 0) (helper (f x) (- n 1)) x)) (define (repeated-f x) (helper x n)) repeated-f)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +16,8 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (cond ((= b 0) a)
+                        ((= a 0) b)
+                        ((= (modulo (max a b) (min a b)) 0) (min a b)) 
+                        (else (gcd (min a b) (modulo (max a b) (min a b))))
+                        ))
--- Lab/lab10/lab10.py
+++ Lab/lab10/lab10.py
@@ -14,20 +14,20 @@ def calc_eval(exp):
     3
     """
     if isinstance(exp, Pair):
-        operator = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
-        operands = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
+        operator = exp.first # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
+        operands = exp.rest # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
         if operator == 'and': # and expressions
             return eval_and(operands)
         elif operator == 'define': # define expressions
             return eval_define(operands)
         else: # Call expressions
-            return calc_apply(___________, ___________) # UPDATE THIS FOR Q2, what is type(operator)?
+            return calc_apply(calc_eval(operator), operands) # UPDATE THIS FOR Q2, what is type(operator)?
     elif exp in OPERATORS:   # Looking up procedures
         return OPERATORS[exp]
     elif isinstance(exp, int) or isinstance(exp, bool):   # Numbers and booleans
         return exp
-    elif _________________: # CHANGE THIS CONDITION FOR Q4 where are variables stored?
-        return _________________ # UPDATE THIS FOR Q4, how do you access a variable?
+    elif exp in bindings: # CHANGE THIS CONDITION FOR Q4 where are variables stored?
+        return bindings[exp] # UPDATE THIS FOR Q4, how do you access a variable?
 
 def calc_apply(op, args):
     return op(args)
@@ -52,6 +52,16 @@ def floor_div(args):
     20
     """
     "*** YOUR CODE HERE ***"
+    def helper(result, args):
+        if args.rest == nil:
+            return result // calc_eval(args.first)
+        else:
+            result = result // calc_eval(args.first)
+            return helper(result, args.rest)
+    if args.rest == nil:
+        return calc_eval(args.first)
+    else:
+        return helper(args.first, args.rest)
 
 scheme_t = True   # Scheme's #t
 scheme_f = False  # Scheme's #f
@@ -75,6 +85,16 @@ def eval_and(expressions):
     """
     "*** YOUR CODE HERE ***"
 
+    if expressions == nil:
+        return scheme_t
+    else:
+        if calc_eval(expressions.first) == scheme_f and type(calc_eval(expressions.first)) == bool:
+            return scheme_f
+        elif expressions.rest == nil:
+            return calc_eval(expressions.first)
+        else:
+            return eval_and(expressions.rest)
+
 bindings = {}
 
 def eval_define(expressions):
@@ -93,7 +113,54 @@ def eval_define(expressions):
     2
     """
     "*** YOUR CODE HERE ***"
+    bindings[expressions.first] = calc_eval(expressions.rest.first)
+    return expressions.first
 
+def addition(args):
+    def helper(result, args):
+        if args.rest == nil:
+            return result + calc_eval(args.first)
+        else:
+            result = result + calc_eval(args.first)
+            return helper(result, args.rest)
+    if args.rest == nil:
+        return calc_eval(args.first)
+    else:
+        return helper(args.first, args.rest)
+def subtraction(args):
+    def helper(result, args):
+        if args.rest == nil:
+            return result - calc_eval(args.first)
+        else:
+            result = result - calc_eval(args.first)
+            return helper(result, args.rest)
+    if args.rest == nil:
+        return calc_eval(args.first)
+    else:
+        return helper(args.first, args.rest)
+    
+def multiplication(args):
+    def helper(result, args):
+        if args.rest == nil:
+            return result * calc_eval(args.first)
+        else:
+            result = result * calc_eval(args.first)
+            return helper(result, args.rest)
+    if args.rest == nil:
+        return calc_eval(args.first)
+    else:
+        return helper(args.first, args.rest)
+def division(args):
+    def helper(result, args):
+        if args.rest == nil:
+            return result / calc_eval(args.first)
+        else:
+            result = result / calc_eval(args.first)
+            return helper(result, args.rest)
+    if args.rest == nil:
+        return calc_eval(args.first)
+    else:
+        return helper(args.first, args.rest)
 OPERATORS = { "//": floor_div, "+": addition, "-": subtraction, "*": multiplication, "/": division }
 
 class Pair:
